7fa703c51b25be4701569a4c3994d7e2622ef29f
origamiwang <2241375541@qq.com>
2024-01-28 19:25:41 +0800

    添加配置文件对ext扩展配置的读取

--- micro/confparser/conf.go
+++ micro/confparser/conf.go
@@ -5,6 +5,9 @@ import (
 	logutil "github.com/OrigamiWang/msd/micro/util/log"
 	"gopkg.in/yaml.v3"
 	"os"
+	"reflect"
+	"strings"
+	"time"
 )
 
 var Conf *Config // 读取配置文件的配置
@@ -17,11 +20,12 @@ type Database struct {
 	Port     int                    `yaml:"port"`
 	User     string                 `yaml:"user"`
 	Password string                 `yaml:"password"`
-	Ext      map[string]interface{} `yaml:",flow"` // 扩展
+	EXT      map[string]interface{} `yaml:",flow"` // 扩展
 }
 
 type Config struct {
-	Dbs []Database `yaml:"databases,flow"`
+	Dbs []Database             `yaml:"databases,flow"`
+	EXT map[string]interface{} `yaml:"ext,flow"`
 }
 
 func init() {
@@ -45,6 +49,104 @@ func LoadConf() *Config {
 		logutil.Error("unmarshal config failed, err: %v", err)
 		return nil
 	}
-	fmt.Println(config)
 	return config
+}
+
+func (c *Config) Ext(keys string, defaultVal ...interface{}) interface{} {
+	res, err := c.ExtSep(keys, ".")
+	if err != nil || res == nil {
+		if len(defaultVal) > 0 {
+			return defaultVal[0]
+		} else {
+			panic(err)
+		}
+	}
+	return res
+}
+
+func (c *Config) ExtSep(keys, sep string) (interface{}, error) {
+	ks := strings.Split(keys, sep)
+	var res interface{}
+	var success, isFinal bool
+	res = c.EXT
+	for _, k := range ks {
+		res, success, isFinal = find(res, k)
+		if !success {
+			return "", fmt.Errorf("no such key: %v", k)
+		} else if isFinal {
+			break
+		}
+	}
+	if success {
+		return res, nil
+	} else {
+		return "", fmt.Errorf("not found the key: %v", keys)
+	}
+}
+
+func find(v interface{}, key interface{}) (res interface{}, success, isFinal bool) {
+	switch m := v.(type) {
+	case map[string]interface{}:
+		res, success = m[key.(string)]
+		isFinal = reflect.TypeOf(res) != nil && reflect.TypeOf(res).Kind() != reflect.Map
+	case map[interface{}]interface{}:
+		res, success = m[key]
+		isFinal = reflect.TypeOf(res) != nil && reflect.TypeOf(res).Kind() != reflect.Map
+	}
+	return
+}
+
+func (c *Config) ExtString(keys string, defaultVal ...interface{}) string {
+	return c.Ext(keys, defaultVal...).(string)
+}
+
+func (c *Config) ExtInt(key string, defaultVal ...interface{}) int {
+	return c.Ext(key, defaultVal...).(int)
+}
+func (c *Config) ExtFloat32(key string, defaultVal ...interface{}) float32 {
+	return c.Ext(key, defaultVal...).(float32)
+}
+func (c *Config) ExtFloat64(key string, defaultVal ...interface{}) float64 {
+	return c.Ext(key, defaultVal...).(float64)
+}
+func (c *Config) ExtBool(key string, defaultVal ...interface{}) bool {
+	return c.Ext(key, defaultVal...).(bool)
+}
+
+func (c *Config) ExtDuration(key string, defaultVal ...interface{}) time.Duration {
+	str := fmt.Sprintf("%v", c.Ext(key, defaultVal...))
+	t, _ := time.ParseDuration(str)
+	return t
+}
+
+func (d *Database) Ext(key string, defaultVal ...interface{}) interface{} {
+	if res, exist := d.EXT[key]; exist {
+		return res
+	}
+	if len(defaultVal) > 0 {
+		return defaultVal[0]
+	}
+	logutil.Error("the key is not exist: %v", key)
+	return ""
+}
+
+func (d *Database) ExtString(key string, defaultVal ...interface{}) string {
+	return d.Ext(key, defaultVal...).(string)
+}
+func (d *Database) ExtInt(key string, defaultVal ...interface{}) int {
+	return d.Ext(key, defaultVal...).(int)
+}
+func (d *Database) ExtFloat32(key string, defaultVal ...interface{}) float32 {
+	return d.Ext(key, defaultVal...).(float32)
+}
+func (d *Database) ExtFloat64(key string, defaultVal ...interface{}) float64 {
+	return d.Ext(key, defaultVal...).(float64)
+}
+func (d *Database) ExtBool(key string, defaultVal ...interface{}) bool {
+	return d.Ext(key, defaultVal...).(bool)
+}
+func (d *Database) ExtDuration(key string, defaultVal ...interface{}) time.Duration {
+	str := fmt.Sprintf("%v", d.Ext(key, defaultVal...))
+	t, _ := time.ParseDuration(str)
+	return t
 }
