9c98e6a8bc7f2b5541de55ce817d3e621f0b8f90
Origami <2241375541@qq.com>
2024-01-30 19:24:59 +0800

    fix

---
+++ gate/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/OrigamiWang/msd/gate/biz"
+	"github.com/OrigamiWang/msd/micro/framework"
+)
+
+func main() {
+	root := framework.NewGinWeb()
+	r := root.Group("/api")
+
+	public := r.Group("/public")
+	// auth := r.Group("/auth")
+
+	biz.Proxy("http://localhost:8081", "/gormdemo", public)
+	biz.Proxy("http://localhost:8082", "/auth", public)
+	root.Run("0.0.0.0:8848")
+}
