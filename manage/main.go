12d906962bf32e71397849b5cf15e19ecbac2f74
Origami <2241375541@qq.com>
2024-01-30 19:13:53 +0800

    rename gorm-demo to user

---
+++ manage/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/OrigamiWang/msd/gorm-demo/handler"
+	"github.com/OrigamiWang/msd/micro/framework"
+	mw "github.com/OrigamiWang/msd/micro/midware"
+)
+
+func main() {
+	root := framework.NewGinWeb()
+	r := root.Group("/")
+	// pprof 性能监
+	//pprof.Register(root.Engine)
+
+	d := root.Group("/debug")
+	{
+		d.GET("/test", mw.PostHandler(handler.Test))
+
+	}
+	{
+		r.GET("/user", mw.PostHandler(handler.GetUserListHandler))
+		r.GET("/user/:id", mw.PostHandler(handler.GetUserByIdHandler))
+		r.PUT("/user/:id", mw.PostHandler(handler.UpdateUserHandler, handler.UserBinder))
+		r.POST("/user", mw.PostHandler(handler.AddUserHandler, handler.UserBinder))
+	}
+	root.Run("localhost:8081")
+}
---
+++ manage/model/dto/user.go
@@ -0,0 +1,9 @@
+package dto
+
+// 等同于dto
+
+type UserReq struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+	Sex  string `json:"sex"`
+}
