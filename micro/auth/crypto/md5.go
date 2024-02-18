fd310474e83f0e5fda059981899c28e2a40fd3e7
Origami <2241375541@qq.com>
2024-02-18 11:47:24 +0800

    md5 result to upper

--- micro/auth/crypto/md5.go
+++ micro/auth/crypto/md5.go
@@ -3,11 +3,12 @@ package crypto
 import (
 	"crypto/md5"
 	"fmt"
+	"strings"
 )
 
 func Md5Encode(rawStr string) string {
 	hash := md5.Sum([]byte(rawStr))
 	md5str := fmt.Sprintf("%x", hash) //将[]byte转成16进制
 	// 32 bit result
-	return md5str
+	return strings.ToUpper(md5str)
 }
