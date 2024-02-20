e48e63b274ceb313add8bd2c3d00a27630b97e2d
Origami <2241375541@qq.com>
2024-02-20 15:57:32 +0800

    remove gate gin web

--- micro/framework/ginweb.go
+++ micro/framework/ginweb.go
@@ -1,7 +1,6 @@
 package framework
 
 import (
-	"github.com/OrigamiWang/msd/micro/midware"
 	"github.com/OrigamiWang/msd/micro/model"
 	"github.com/gin-gonic/gin"
 )
@@ -11,7 +10,6 @@ func New() *gin.Engine {
 	g := gin.New()
 	g.Use(gin.Logger())
 	g.Use(gin.Recovery())
-	g.Use(midware.CheckGateApiKeyMiddleware())
 	// FIXME: add customized middleware
 	// g.Use(TraceExtractor())
 	// g.Use(Trace())
@@ -21,19 +19,4 @@ func New() *gin.Engine {
 
 func NewGinWeb() *model.Ginweb {
 	return &model.Ginweb{New()}
-}
-func NewGate() *gin.Engine {
-	gin.SetMode(gin.DebugMode)
-	g := gin.New()
-	g.Use(gin.Logger())
-	g.Use(gin.Recovery())
-	g.Use(midware.AddGateApiKeyMiddleware())
-	return g
-}
-
-// NewGateGinWeb use for gate
-// this not use CheckGateApiKeyMiddleware to check the api key
-// but use AddGateApiKeyMiddleware to add api key to the request
-func NewGateGinWeb() *model.Ginweb {
-	return &model.Ginweb{NewGate()}
 }
