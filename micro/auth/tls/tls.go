299c0f611e10fe74fbefbe6537aa5e831873661c
Origami <2241375541@qq.com>
2024-02-21 14:26:21 +0800

    add tls config

--- micro/auth/tls/tls.go
+++ micro/auth/tls/tls.go
@@ -1,1 +1,42 @@
 package tls
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"os"
+
+	logutil "github.com/OrigamiWang/msd/micro/util/log"
+)
+
+var (
+	TlsConfig = &tls.Config{}
+)
+
+func init() {
+	var err error
+	TlsConfig, err = initTlsConfig()
+	if err != nil {
+		logutil.Error("init cls config failed, err: %v", err)
+		panic(err.Error())
+	}
+}
+func initTlsConfig() (*tls.Config, error) {
+	caCert, err := os.ReadFile("conf/ca.crt")
+	if err != nil {
+		logutil.Error("read ca.crt error: %v", err)
+		return nil, err
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+	clientCert, err := tls.LoadX509KeyPair("conf/client.crt", "conf/client.key")
+	if err != nil {
+		logutil.Error("read client.crt or client.key error: %v", err)
+		return nil, err
+	}
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{clientCert},
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		ClientCAs:    caCertPool,
+	}
+	return tlsConfig, nil
+}
