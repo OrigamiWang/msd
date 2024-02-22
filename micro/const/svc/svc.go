5701a7be29ac9da249ec3c73c3fe9e5a27edfde6
Origami <2241375541@qq.com>
2024-02-22 15:33:33 +0800

    modify svc name

--- micro/const/svc/svc.go
+++ micro/const/svc/svc.go
@@ -4,6 +4,6 @@ const (
 	MICRO           = "micro"
 	MANAGE          = "manage"
 	MANAGE2         = "manage2"
-	CONF_CENTER     = "conf-center"
-	REGISTER_CENTER = "register-center"
+	CONF_CENTER     = "confcenter"
+	REGISTER_CENTER = "registercenter"
 )
